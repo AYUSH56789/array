5c1d3d7bd7b4a59cb7d1c09b78a1ff050cd78d73
Ayush Singh <ayushsingh46026@gmal.com>
2024-06-09 13:35:31 +0530

    done

--- easy-problems/problem12.java
+++ easy-problems/problem12.java
@@ -1,10 +1,30 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+
 
 public class problem12 {
-    // brute force approach: time complexity O(N) with space complexity O(N)
-    public static void bruteForceSolution(int arr[]){
-        // pending...
+    // brute force approach: time complexity O(2N) ~= O(N) with space complexity O(N)
+    public static int bruteForceSolution(int arr[]){
+        // step1: assume result is -1;
+        int result=-1;
+        // step2: create hashmap 
+        HashMap<Integer,Integer> map=new HashMap<>();
+        // step3: iterate on the array and store all element as key and store its frequency as its value
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i], 0)+1 );
+        }
+        // step4: iterate on the array that get map value by passing key and check if getting value is 1 than result is that key
+        for (int i = 0; i < arr.length; i++) {
+            if(map.get(arr[i])==1){
+                result= arr[i];
+            }
+        }
+        // step5: return result
+        return result;
     }
+
+
     // optimal approach: time complexity O(N) with space complexity O(1)
     public static void optimalSolution(int arr[]){
         int xor=0;
--- easy-problems/problem13.java
+++ easy-problems/problem13.java
@@ -1,6 +1,5 @@
 public class problem13 {
     public static void main(String[] args) {
-        // pending
-        // problem 8 and 12
+        
     }
 }
