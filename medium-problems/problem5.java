11a59475ed9a8569457db02830ac9087e659da29
Ayush Singh <ayushsingh46026@gmal.com>
2024-06-15 09:04:57 +0530

    done

--- medium-problems/problem5.java
+++ medium-problems/problem5.java
@@ -1,5 +1,51 @@
 public class problem5 {
+    // optimal solution: time complexity O(N) with O(1)
+    public static int optimalSolution(int prices[]){
+        int profit=0;
+        int buy=prices[0];
+        int sell=0;
+        for (int i = 0; i < prices.length; i++) {
+            int tempProfit=prices[i]-buy;
+            // profit=Math.max(profit,tempProfit);
+            if(tempProfit>profit){
+                profit=tempProfit;
+                sell=prices[i];
+            }
+            buy=Math.min(buy,prices[i]);
+        }
+        System.out.println("profit price->"+profit+", buy price-> "+buy+", sell price-> "+sell);
+        return profit;
+    }
     public static void main(String[] args) {
-        
+        int prices[]={7,1,5,3,6,4};
+        optimalSolution(prices);
     }
 }
+
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+*/
